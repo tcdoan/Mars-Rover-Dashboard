64a36b22daca8910e02b09c1c976102c68822ddb
Thanh Doan <doant@outlook.com>
2021-07-31 23:07:15 -0700

    Implement Mars Rover Dashboard.

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,126 @@
-let store = {
+const store = Immutable.Map({
     user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+    currentRoverData: "",
+    rovers: ['Curiosity', 'Opportunity', 'Spirit']
+});
 
 // add our markup to the page
 const root = document.getElementById('root')
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+const updateStore = (store, rover) => {
+    store = store.set('currentRoverData', Immutable.fromJS(rover));
+    
+    // whenever store has new rover, update photos
+    const roverName = store.get('currentRoverData').get('name')
+    const roverDate = store.get('currentRoverData').get('max_date')
+    fetchAndRenderPhotos(roverName, roverDate);
+};
+
+// ------------------------------------------------------  API CALLS
+const fetchManifest= (name, store) => {
+    fetch(`/rover/${name}`)
+        .then(res => res.json())
+        .then(rover => {       
+            renderStats(Immutable.Map(rover));
+            updateStore(store, rover);
+        });
+};
+
+const renderStats = rover => {
+    document.getElementById('stats').innerHTML = `
+        <div><span class="key">Status: </span> <span class="val">${rover.get('status')}</span></div>
+        <div><span class="key">Launch Date: </span><span class="val">${new Date(rover.get('launch_date')).toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}</span></div>
+        <div><span class="key">Landing Date: </span><span class="val">${new Date(rover.get('landing_date')).toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}</span></div>
+        `;
+};
+
+const fetchAndRenderPhotos = (name, date) => {
+    fetch(`/rover/${name}/${date}`)
+        .then(res => res.json())
+        .then(photos => {
+            const photosDiv = document.getElementById('photos')
+            renderElement(photosDiv, createPhotos, photos);
+        });
 }
 
-const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
+// HIGHER ORDER FUNCTION 1
+// render HTML elements (rendered from an array) onto a parent HTML element
+const renderElement = (element, renderFunc, arr) => {
+    element.innerHTML = '';
+    element.append(...renderFunc(arr));
+};
 
+const createPhotos = (photos) => {
+    const photoGroups = photos.map(photo => {
+        const photoGroup = document.createElement('div');
+        const htmlPhoto = document.createElement('img');
+        htmlPhoto.src = photo.img_src;
+        photoGroup.appendChild(htmlPhoto);
+        const photoDate = document.createElement('p');
+        const date = new Date(photo.earth_date);
+        photoDate.innerText = `Date Taken: ${date.toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}`
+        photoGroup.appendChild(photoDate);
+        return photoGroup;
+    });
+    return photoGroups;
+};
+
+async function render(root, store) {
+    root.innerHTML = App();
+    const navElement = document.getElementsByTagName('nav')[0]
+    
+    // renderElement is high order function because its second argument is 
+    // renderButtons function.
+    renderElement(navElement, createButtons, store.get('rovers'));
+    
+    function buttonClickHandler(button) {
+        document.querySelector('.selected').classList.remove('selected');
+        button.classList.add('selected');
+        fetchManifest(button.innerText, store);
+    }
+
+    addRoverListeners(buttonClickHandler);
+};
+
+function createButtons(roverNames) {
+    const buttons = roverNames.map((name, idx) => {
+        const button = document.createElement('button');
+        if (idx === 2) {
+            button.classList.add('selected');
+            fetchManifest(name, store);
+        }
+        button.innerText = name;
+        return button;
+    });
+    return buttons;
+};
+
+// add button listeners to the rovers 
+// HIGHER ORDER FUNCTION 2
+function addRoverListeners(clickEventHandler) {
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', () => {
+            clickEventHandler(button);
+        });
+    });
+};
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+const App = () => {
+    return `<header>
+                <h1>Mars Rover Dashboard</h1>
+                <p>Click on a Mars Rover name to view its information and photos.</p>
+            </header>
+            <nav>
+            </nav>
+            <div id="stats">
+            </div>
+            <div id="photos">
+            </div>
+            <footer>Rover Information Taken From <a href="https://www.nasa.gov">NASA</a></footer>`;
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
-
-// ------------------------------------------------------  COMPONENTS
-
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
-    return `
-        <h1>Hello!</h1>
-    `
-}
-
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
-
-// ------------------------------------------------------  API CALLS
-
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
-    return data
-}
+    render(root, store);
+});
